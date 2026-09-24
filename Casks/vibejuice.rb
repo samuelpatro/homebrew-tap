403eb17dd79611cc5d9452965bbe2e17b304fac9
vibejuice-release <release@vibejuice.invalid>
2026-09-24 17:46:44 +0000

    vibejuice 0.4.17

--- Casks/vibejuice.rb
+++ Casks/vibejuice.rb
@@ -1,6 +1,6 @@
 cask "vibejuice" do
-  version "0.4.16"
-  sha256 "a402112541c0e027bdd04ed61572fcb0a95cfba75ce68abf433602a33415892c"
+  version "0.4.17"
+  sha256 "4cc653c2970f3827286bf77fdbd73b7fb946531cf71b7d1b2d500b9a2a9b615c"
 
   url "https://github.com/samuelpatro/vibejuice/releases/download/v#{version}/VibeJuice-#{version}.dmg"
   name "VibeJuice"
@@ -10,7 +10,7 @@ cask "vibejuice" do
   depends_on macos: :tahoe
 
   # Unsigned build: drop the quarantine flag before install so Gatekeeper does not block it.
-  preflight_steps do
+  preflight do
     system_command "/usr/bin/xattr", args: ["-cr", "#{staged_path}/VibeJuice.app"]
   end
 
